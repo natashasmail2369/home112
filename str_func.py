e0f647072e47890583832093945d7b73cf14463e
natalia236 <natashawinx97@gmail.com>
2024-05-22 23:22:49 +0300

    add str_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def make_uppercase(string):
+    """Делает заглавными все буквы слов"""
     return string.upper()
+
+
+def make_titlecase(string):
+    """Делает заглавными первые буквы слов, вот это даааа"""
+    return string.title()
